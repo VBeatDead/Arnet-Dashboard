03d953346fa1f450337557cca1b70c149333b1d0
VBeatDead <77430117+VBeatDead@users.noreply.github.com>
2024-07-11 15:19:44 +0700

    fix overlay

--- resources/views/denah/index.blade.php
+++ resources/views/denah/index.blade.php
@@ -5,27 +5,27 @@
 
 @section('content')
 
-    <!-- TABLE -->
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <h6 class="card-title text-uppercase">Data Denah STO</h6>
-                    <div>
-                        <a href="/adddenah" class="btn btn-primary mb-4 mt-3">
-                            <i class="bi bi-plus me-3"></i>Tambah Denah STO
-                        </a>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table table-striped table-bordered w-100" id="example">
-                            <thead class="text-center">
-                                <tr>
-                                    <th>No</th>
-                                    <th>Lokasi STO</th>
-                                    <th>Denah</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
+<!-- TABLE -->
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h6 class="card-title text-uppercase">Data Denah STO</h6>
+                <div>
+                    <a href="/adddenah" class="btn btn-primary mb-4 mt-3">
+                        <i class="bi bi-plus me-3"></i>Tambah Denah STO
+                    </a>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered w-100" id="example">
+                        <thead class="text-center">
+                            <tr>
+                                <th>No</th>
+                                <th>Lokasi STO</th>
+                                <th>Denah</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
                         <tbody>
                         @foreach ($denah as $d)
                         <tr>
@@ -35,9 +35,13 @@
                                 @php
                                     $convertedImageUrl = asset($d->converted_image);
                                 @endphp
-                                <a href="javascript:void(0);" onclick="showImage('{{ $convertedImageUrl }}')">
-                                    <img src="{{ $convertedImageUrl }}" alt="{{ $d->name }}" class="img-fluid" style="max-width: 100px; max-height: 100px;">
-                                </a>
+                                @if (!empty($d->converted_image))
+                                    <a href="javascript:void(0);" onclick="showImage('{{ $convertedImageUrl }}')">
+                                        <img src="{{ $convertedImageUrl }}" alt="{{ $d->name }}" class="img-fluid" style="max-width: 100px; max-height: 100px;">
+                                    </a>
+                                @else
+                                    <p>No Image Available</p>
+                                @endif
                             </td>
                             <td class="text-center">
                                 <a href="{{ route('denah.edit', $d->id) }}" class="btn btn-warning">
@@ -49,20 +53,21 @@
                             </td>
                         </tr>
                         @endforeach
-                        </table>
-                    </div>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-    <!-- END OF TABLE -->
-
-{{-- IMAGE OVERLAY --}}
-<div id="imageOverlay" class="image-overlay">
-    <span class="close-btn" onclick="closeImageOverlay()">&times;</span>
-    <img id="overlayImage" src="{{ $convertedImageUrl }}" class="overlay-image">
 </div>
-{{-- END OF IMAGE OVERLAY --}}
+<!-- END OF TABLE -->
+
+<!-- IMAGE OVERLAY -->
+<div id="imageOverlay" class="image-overlay" style="display: none;">
+    <span class="close-btn" onclick="closeImageOverlay()">&times;</span>
+    <img id="overlayImage" src="" class="overlay-image">
+</div>
+<!-- END OF IMAGE OVERLAY -->
 
 <!-- DELETE MODAL -->
 <div class="modal fade" id="deleteModal">
@@ -75,8 +80,7 @@
             <div class="modal-body">
                 <p>Are you sure you want to delete this item?</p>
             </div>
-
-            <div class="modal-footer">                
+            <div class="modal-footer">
                 <a href="javascript:void(0)" class="btn btn-danger">Delete</a>
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
             </div>
@@ -86,6 +90,8 @@
 <!-- END OF DELETE MODAL -->
 
 @endsection
+
+@section('scripts')
 <script>
     const handleDelete = (id) => document.querySelector('#deleteModal .modal-footer a').href = 'denah/d' + id;
 
@@ -98,3 +104,4 @@
         document.getElementById('imageOverlay').style.display = "none";
     }
 </script>
+@endsection
